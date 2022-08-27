82675f2a3299dc5c92ae9802e7d94b02a46a4a99
Pedro Almeida <100890704+Brownlius@users.noreply.github.com>
2022-08-27 12:23:35 -0400

    .

---
+++ Models/Movie.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace FilmesAPI.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Director { get;set; }
+        public string Genre { get; set; }
+        public int Duration { get; set; }
+    }
+}
